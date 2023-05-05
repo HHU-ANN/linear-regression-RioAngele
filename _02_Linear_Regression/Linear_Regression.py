186a8e43acd246e42d78136b247f154977152b21
RioAngele <77475226+RioAngele@users.noreply.github.com>
2023-05-05 21:41:54 +0800

    8

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -13,7 +13,7 @@ def ridge(data):
     X,y = read_data()
     # weight = np.dot(np.linalg.inv((np.dot(x.T,x)+np.dot(alpha,np.eye(6)))),np.dot(x.T,y))
     XtX = np.dot(X.T, X)
-    n = X.shape[0]
+    n = X.shape[0]-1
     return data @ np.linalg.solve(XtX , np.dot(X.T, y))
     # return weight @ data
 
@@ -23,7 +23,7 @@ def lasso(data):
     weight = data
     y = np.dot(weight, x.T)
     alpha = 2.1
-    rate = 0.00000000000007
+    rate = 0.0000000007
     weight =model_lasso(x,Y,alpha,rate,weight)
     return weight @ data
 
