5952f79e0b8d8f3a9e402a138f3549dbcb1485bb
RioAngele <77475226+RioAngele@users.noreply.github.com>
2023-05-05 21:54:57 +0800

    8

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -20,7 +20,7 @@ def lasso(data):
     x, Y = read_data()
     weight = data
     y = np.dot(weight, x.T)
-    alpha = 1.8
+    alpha = 30
     rate = 0.0000007
     weight =model_lasso(x,Y,alpha,rate,weight)
     return weight @ data
