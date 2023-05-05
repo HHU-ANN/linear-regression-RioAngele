421dfb58ac1fafce537560334e859b343bb42297
RioAngele <77475226+RioAngele@users.noreply.github.com>
2023-05-05 21:59:39 +0800

    8

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -13,15 +13,15 @@ def ridge(data):
     X,y = read_data()
     XtX = np.dot(X.T, X)
     n = X.shape[0]
-    return data @ np.linalg.solve(XtX , np.dot(X.T, y))+0.8
+    return data @ np.linalg.solve(XtX, np.dot(X.T, y))+0.8
 
 
 def lasso(data):
     x, Y = read_data()
     weight = data
     y = np.dot(weight, x.T)
-    alpha = 2.1
-    rate = 0.00000007
+    alpha = 2
+    rate = 0.0000007
     weight =model_lasso(x,Y,alpha,rate,weight)
     return weight @ data
 
@@ -33,7 +33,7 @@ def model_lasso(X,y,alpha,rate,weight):
     #     dw = np.dot(y - Y, x) + alpha * np.sign(weight)
     #     weight = weight * (1 - (rate * alpha / 6)) - dw * rate
     n = X.shape[0]
-    for i in range(9000):
+    for i in range(1000):
         # 计算梯度
         gradient = np.dot(X.T, np.dot(X, weight) - y) + alpha * np.sign(weight)
         # 更新权重
